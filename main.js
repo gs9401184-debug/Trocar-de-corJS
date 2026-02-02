9f119c800123d0f9e893e87c05555fe5d38fc141
GABRIEL SOUSA RODRIGUES DE MELO <25203630@CFP127ALUNO.LOCAL>
2026-02-02 11:18:34 -0300

    Finalizando o projeto de teste e aprendizagem sobre cores

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert(oi)
+'use strict'
+
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+const coresTraduzidas = {
+    'azul': 'blue',
+    'vermelho': 'red',
+    'verde': 'green',
+    'amarelo': 'yellow'
+}
+
+function trocarCor() {
+    const inputCor = document.getElementById('cor').value.toLowerCase()
+
+    const corFinal = coresTraduzidas[inputCor] || inputCor
+
+    document.body.style.backgroundColor = corFinal
+}
+
+botaoTrocarCor.addEventListener('click', trocarCor)
